e3ce0d93bb5da5fc6b9e3f4f0cb3e2a011c48700
Victor Tarus <Tarus@NC-tarus-FVFC40T0L414.local>
2022-10-14 09:46:06 +0300

    Feature Dynamo db table created

--- infrastructure/SpaceStack.ts
+++ infrastructure/SpaceStack.ts
@@ -1,11 +1,18 @@
+import { GenericTable } from './GenericTable';
 //contain all logic
 import {Stack, StackProps} from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 import { Code, Function as LambdaFunction, Runtime } from 'aws-cdk-lib/aws-lambda';
 import {join} from 'path'
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class SpaceStack extends Stack{
-
+private api = new RestApi(this,'SpaceApi');
+private spacesTable = new GenericTable(
+    "SpacesTable",
+    "spaceId",
+    this
+)
     constructor(scope:Construct,id:string,props?:StackProps){
         super(scope,id,props)
 
@@ -14,6 +21,14 @@ export class SpaceStack extends Stack{
             code:Code.fromAsset(join(__dirname,'..','services','hello')),
             handler:'hello.main'
         })
+
+// Hello Api lambda integration :Linking together api gateway and lambda
+const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+const helloLambdaResource = this.api.root.addResource('hello');
+helloLambdaResource.addMethod('GET',helloLambdaIntegration);
+
+
+
     }
 
 }
